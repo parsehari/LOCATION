af02c72771b4d84687b19dd898e4d512ee7c6852
parsehari <parse.harsh7.hp@gmail.com>
2018-11-20 17:11:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {geolocated} from 'react-geolocated';
 
-class App extends Component {
+class Demo extends React.Component {
+
+  componentWillMount(){
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return !this.props.isGeolocationAvailable
+      ? <div>Your browser does not support Geolocation</div>
+      : !this.props.isGeolocationEnabled
+        ? <div>Geolocation is not enabled</div>
+        : this.props.coords
+          ? <table>
+            <tbody>
+              <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
+              <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
+              <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
+              <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
+              <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
+            </tbody>
+          </table>
+          : <div>Getting the location data&hellip; </div>;
   }
 }
-
-export default App;
+ 
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(Demo);
